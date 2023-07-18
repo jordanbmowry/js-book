7cf9d84499f7fef4609a08997beb63d48915369f
Joran Mowry <jordan.mowry@gmail.com>
2023-07-18 15:13:45 -0400

    safely handle untrusted code through iframe

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 function App() {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ function App() {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -30,9 +32,35 @@ function App() {
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: { 'process.env.NODE_ENV': '"production"', global: 'window' },
     });
-    // console.log(result);
-    setCode(result.outputFiles[0].text);
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+    window.addEventListener('message', e => {
+      try {
+        eval(e.data);
+      } catch (err) {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+        console.error(err)
+      }
+    }, false);
+    </script>
+  </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -43,7 +71,12 @@ function App() {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
     </div>
   );
 }
